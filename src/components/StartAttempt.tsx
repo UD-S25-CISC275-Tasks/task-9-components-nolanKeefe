378e50c2f02d8c62c433c81a64f1c7231cf390dd
nolanKeefe <npkeefe@udel.edu>
2025-03-07 19:06:31 -0500

    ChangeType reveal answer and start attempt completed fixed package issues

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,53 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): React.JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempts, setValue] = useState<number>(4);
+    const [inProgress, setProgress] = useState<boolean>(false);
+    function flipProgress(): void {
+        // Set visible to be the logical opposite of its previous value
+        setProgress(!inProgress);
+    }
+    function startClick(): void {
+        setValue(attempts - 1);
+        flipProgress();
+    }
+    return (
+        <span>
+            {attempts <= 0 || inProgress ? ( //if attempts is less than 0 and inprogress is true it should be a disabled button
+                <Button
+                    onClick={() => {
+                        startClick();
+                    }}
+                    disabled={true}
+                >
+                    Start Quiz
+                </Button>
+            ) : (
+                <Button
+                    onClick={() => {
+                        startClick();
+                    }}
+                >
+                    Start Quiz
+                </Button>
+            )}{" "}
+            attempts: {attempts}
+            <Button
+                onClick={() => {
+                    flipProgress();
+                }}
+                disabled={!inProgress}
+            >
+                Stop Quiz
+            </Button>
+            <Button
+                onClick={() => {
+                    setValue(attempts + 1);
+                }}
+                disabled={inProgress}
+            >
+                Mulligan
+            </Button>
+        </span>
+    );
 }
